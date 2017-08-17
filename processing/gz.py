5dfb9472963d9244b001d41c1f2750174e89bcb5
KwatME <kwat.medetgul.ernar@gmail.com>
2017-08-17 12:22:47 -0700

    gz.py

--- processing/gz.py
+++ processing/gz.py
@@ -1,80 +1,61 @@
 import gzip
 from os import remove, rename
-from os.path import isfile
 
 from Bio import bgzf
 
 from .helper.helper.subprocess_ import run_command
 
 
-def bgzip_tabix(file_path):
-    """
-    bgzip and tabix <file_path>.
-    :param file_path: str
-    :return: str
-    """
-
-    if file_path.endswith('.gz'):
-
-        if not isfile('{}.tbi'.format(file_path)):  # Not tabixed
-            tabix(file_path)
-
-        return file_path
-
-    else:
-
-        bgzip_output_file_path = bgzip(file_path)
-        tabix(bgzip_output_file_path)
-
-    return bgzip_output_file_path
-
-
 def bgzip(file_path):
     """
-    bgzip <file_path>.
-    :param file_path: str
-    :return: str
+    bgzip file_path.
+    Arguments:
+        file_path (str):
+    Returns:
+        None
     """
 
     command = 'bgzip -f {}'.format(file_path)
 
     run_command(command)
 
-    return '{}.gz'.format(file_path)
 
-
-def tabix(file_path):
+def tabix(bgzipped_file_path):
     """
-    tabix <file_path>.
-    :param file_path: str
-    :return: str
+    tabix bgzipped_file_path.
+    Arguments:
+        bgzipped_file_path (str):
+    Returns:
+        None
     """
 
-    command = 'tabix -f {}'.format(file_path)
+    command = 'tabix -f {}'.format(bgzipped_file_path)
 
     run_command(command)
-
-    return '{}.tbi'.format(file_path)
 
 
 def convert_gzipped_to_bgzipped(gzipped_file_path):
     """
     Convert gzipped file to bgzipped file.
-    :param gzipped_file_path: str
-    :return: str
+    Arguments:
+        gzipped_file_path (str):
+    Returns:
+        None
     """
 
-    temp_file_path = '{}.convert_gzipped_to_bgzipped'.format(gzipped_file_path)
+    tmp_file_path = '{}.convert_gzipped_to_bgzipped.tmp'.format(
+        gzipped_file_path)
 
+    # Read from gzipped file path
     with gzip.open(gzipped_file_path, 'rt') as gzipped_file:
 
-        with bgzf.open(temp_file_path, 'wt') as temp_bgzipped_file:
-
+        # Write to tmp file path
+        with bgzf.open(tmp_file_path, 'wt') as tmp_bgzipped_file:
             for line in gzipped_file:
-                temp_bgzipped_file.write(line)
+                tmp_bgzipped_file.write(line)
 
+    # Remove gzipped file path
     remove(gzipped_file_path)
 
-    rename(temp_file_path, gzipped_file_path)
-
-    return gzipped_file_path
+    # Rename tmp file path to gzipped file path
+    rename(tmp_file_path, gzipped_file_path)

e1de3ec647c4cd97aa946c5a51d9cf66f5a3ad89
KwatME <kwat.medetgul.ernar@gmail.com>
2017-10-03 14:44:05 -0700

    1

--- process.py
+++ process.py
@@ -1,20 +1,23 @@
 from argparse import ArgumentParser
 
+# ==============================================================================
 # Set up arguments
+# ==============================================================================
 parser = ArgumentParser()
+
 parser.add_argument('process', help='Process name.')
+
 parser.add_argument('-vcf', '--vcf-file-path', help='.vcf file path.')
 
+# ==============================================================================
 # Parse arguments
+# ==============================================================================
 args = parser.parse_args()
 
 process = args.process
+
 vcf_file_path = args.vcf_file_path
 
-if process == 'vcf.snpeff':
-    from sequencing_process.process_vcf import snpeff
-
-    snpeff(vcf_file_path)
-
-elif process == 'fastq.hisat2':
-    from sequencing_process.process_fastq import hisat2
+# ==============================================================================
+# Process
+# ==============================================================================

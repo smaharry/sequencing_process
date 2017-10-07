f48f1b97afdf0d62537cd80baf2c943970c59235
KwatME <kwat.medetgul.ernar@gmail.com>
2017-10-07 14:45:32 -0700

    Faster?

--- sequencing_process/process_sam.py
+++ sequencing_process/process_sam.py
@@ -1,22 +1,28 @@
 from .support.support.subprocess_ import run_command
 
 
-def call_variants_on_bam(bam_file_path, fasta_file_path):
+def call_variants_on_bam(bam_file_path, fasta_file_path, n_jobs=1):
     """
     Call variants on .bam file with freebayes.
     Arguments:
         bam_file_path (str):
         fasta_file_path (str): reference .fasta file
+        n_jobs (int):
     Returns:
         str:
     """
 
-    output_vcf_gz_file_path = bam_file_path + '.freebayes.vcf.gz'
+    output_vcf_file_path = bam_file_path + '.freebayes.vcf'
 
-    command = 'freebayes --fasta-reference {} {} | bgzip -fc > {} ; tabix -f {}'.format(
-        fasta_file_path, bam_file_path, output_vcf_gz_file_path,
-        output_vcf_gz_file_path)
+    # command = 'freebayes --fasta-reference {} {} | bgzip -fc -@ {} > {}; tabix -f {}'.format(
+    command = 'freebayes -f {} {} > {}; bgzip -f -@ {}; tabix -f {}'.format(
+        fasta_file_path,
+        bam_file_path,
+        output_vcf_file_path,
+        n_jobs,
+        output_vcf_file_path + '.gz',
+        output_vcf_file_path + '.gz', )
 
     run_command(command)
 
-    return output_vcf_gz_file_path
+    return output_vcf_file_path + '.gz'

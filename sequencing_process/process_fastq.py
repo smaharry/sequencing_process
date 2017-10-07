6d19ff32b5563bb9a3483a8d802d1719295ea01d
KwatME <kwat.medetgul.ernar@gmail.com>
2017-10-07 13:18:13 -0700

    HISAT2 --> Samtools

--- sequencing_process/process_fastq.py
+++ sequencing_process/process_fastq.py
@@ -7,7 +7,7 @@ def align_dna_fastq_with_hisat2(hisat2_index_file_path_prefix,
                                 paired_reverse_fastq_file_path=None,
                                 n_jobs=1):
     """
-    Align unpaired or paired forward and reverse DNA .fastq files.
+    Align unpaired or paired DNA .fastq files.
     Arguments:
         hisat2_index_file_path_prefix (str):
         unpaired_fastq_file_path (str):
@@ -20,25 +20,23 @@ def align_dna_fastq_with_hisat2(hisat2_index_file_path_prefix,
 
     if unpaired_fastq_file_path:
         sample_command = '-U {}'.format(unpaired_fastq_file_path)
-        output_sam_file_path = unpaired_fastq_file_path + '.hisat2.sam'
+        output_bam_file_path = unpaired_fastq_file_path + '.sam'
 
     elif paired_forward_fastq_file_path and paired_reverse_fastq_file_path:
         sample_command = '-1 {} -2 {}'.format(paired_forward_fastq_file_path,
                                               paired_reverse_fastq_file_path)
-        output_sam_file_path = paired_forward_fastq_file_path + '.hisat2.sam'
+        output_bam_file_path = paired_forward_fastq_file_path + '.sam'
 
     else:
-        raise ValueError(
-            'Need unpaired or paired forward and reverse DNA .fastq file paths.'
-        )
+        raise ValueError('Need unpaired or paired DNA .fastq file paths.')
 
-    command = 'hisat2 --no-spliced-alignment {} -x {} -S {} -p {}'.format(
-        sample_command, hisat2_index_file_path_prefix, output_sam_file_path,
-        n_jobs)
+    command = 'hisat2 --no-spliced-alignment {0} -x {1} -p {2} | samtools view -Sb -@ {2} > {3}'.format(
+        sample_command, hisat2_index_file_path_prefix, n_jobs,
+        output_bam_file_path)
 
     run_command(command)
 
-    return output_sam_file_path
+    return output_bam_file_path
 
 
 def align_rna_fastq_with_hisat2(hisat2_index_file_path_prefix,
@@ -47,7 +45,7 @@ def align_rna_fastq_with_hisat2(hisat2_index_file_path_prefix,
                                 paired_reverse_fastq_file_path=None,
                                 n_jobs=1):
     """
-    Align unpaired or paired forward and reverse RNA .fastq files.
+    Align unpaired or paired RNA .fastq files.
     Arguments:
         hisat2_index_file_path_prefix (str):
         unpaired_fastq_file_path (str):
@@ -60,22 +58,20 @@ def align_rna_fastq_with_hisat2(hisat2_index_file_path_prefix,
 
     if unpaired_fastq_file_path:
         sample_command = '-U {}'.format(unpaired_fastq_file_path)
-        output_sam_file_path = unpaired_fastq_file_path + '.hisat2.sam'
+        output_bam_file_path = unpaired_fastq_file_path + '.bam'
 
     elif paired_forward_fastq_file_path and paired_reverse_fastq_file_path:
         sample_command = '-1 {} -2 {}'.format(paired_forward_fastq_file_path,
                                               paired_reverse_fastq_file_path)
-        output_sam_file_path = paired_forward_fastq_file_path + '.hisat2.sam'
+        output_bam_file_path = paired_forward_fastq_file_path + '.bam'
 
     else:
-        raise ValueError(
-            'Need unpaired or paired forward and reverse RNA .fastq file paths.'
-        )
+        raise ValueError('Need unpaired or paired RNA .fastq file paths.')
 
-    command = 'hisat2 --dta-cufflinks {} -x {} -S {} -p {}'.format(
-        sample_command, hisat2_index_file_path_prefix, output_sam_file_path,
-        n_jobs)
+    command = 'hisat2 --dta-cufflinks {0} -x {1} -p {2} | samtools view -Sb -@ {2} > {3}'.format(
+        sample_command, hisat2_index_file_path_prefix, n_jobs,
+        output_bam_file_path)
 
     run_command(command)
 
-    return output_sam_file_path
+    return output_bam_file_path
--- sequencing_process/process_sam.py
+++ sequencing_process/process_sam.py
@@ -2,9 +2,9 @@ from .process_gz import bgzip, tabix
 from .support.support.subprocess_ import run_command
 
 
-def samtools_sam_to_bam(sam_file_path, n_jobs=1):
+def convert_sam_to_bam(sam_file_path, n_jobs=1):
     """
-    Compress .sam file to .bam file.
+    Convert .sam file to .bam file with samtools.
     Arguments:
         sam_file_path (str):
         n_jobs (int):
@@ -12,7 +12,7 @@ def samtools_sam_to_bam(sam_file_path, n_jobs=1):
         str:
     """
 
-    output_bam_file_path = sam_file_path + '.samptools_sam_to_bam.bam'
+    output_bam_file_path = sam_file_path + '.bam'
 
     command = 'samtools view -Sb -@ {} {} > {}'.format(n_jobs, sam_file_path,
                                                        output_bam_file_path)
@@ -22,9 +22,9 @@ def samtools_sam_to_bam(sam_file_path, n_jobs=1):
     return output_bam_file_path
 
 
-def freebayes(bam_file_path, fasta_file_path):
+def call_variants(bam_file_path, fasta_file_path):
     """
-    Call variants on .bam file.
+    Call variants on .bam file with freebayes.
     Arguments:
         bam_file_path (str):
         fasta_file_path (str): reference .fasta file

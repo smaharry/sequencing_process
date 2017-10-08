b318df4c526499b66b21ae1658866febad570b8c
KwatME <kwat.medetgul.ernar@gmail.com>
2017-10-07 23:04:51 -0700

    Patch until samtools 1.6 is out

--- sequencing_process/process_bam.py
+++ sequencing_process/process_bam.py
@@ -15,9 +15,11 @@ def mark_duplicates_in_bam_with_samtools(bam_file_path, n_jobs):
 
     output_bam_file_path = bam_file_path + '.mark_duplicates_in_bam_with_samtools.bam'
 
-    command = 'samtools sort -n -@ {} {} | samtools fixmate -m -@ {} | samtools sort -@ {} | samtools markdup -@ {} > {}; samtools index -@ {} {}'.format(
-        n_jobs, bam_file_path, n_jobs, n_jobs, n_jobs, output_bam_file_path,
-        n_jobs, output_bam_file_path)
+    # command = 'samtools sort -n -@ {} {} | samtools fixmate -m -@ {} | samtools sort -@ {} | samtools markdup -@ {} > {}; samtools index -@ {} {}'.format(
+    #     n_jobs, bam_file_path, n_jobs, n_jobs, n_jobs, output_bam_file_path,
+    #     n_jobs, output_bam_file_path)
+    command = 'samtools rmdup -@ {} {} > {}'.format(n_jobs, bam_file_path,
+                                                    output_bam_file_path)
 
     run_command(command)
 

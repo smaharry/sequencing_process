9a5d7e4017b17ebc879017c1083b6a0ee15763de
KwatME <kwat.medetgul.ernar@gmail.com>
2017-10-07 17:06:42 -0700

    Push

--- sequencing_process/process_sam.py
+++ sequencing_process/process_sam.py
@@ -1,12 +1,41 @@
+from . import CHROMOSOMES
+from .support.support.multiprocess import multiprocess
 from .support.support.subprocess_ import run_command
 
 
-def call_variants_on_bam(bam_file_path, fasta_file_path, n_jobs=1):
+def call_variants_on_bam_with_multiprocess(bam_file_path,
+                                           fasta_file_path,
+                                           n_jobs,
+                                           chromosomes=CHROMOSOMES):
+    """
+    Call variants on .bam file using multiprocess with freebayes.
+    Arguments:
+        bam_file_path (str):
+        fasta_file_path (str): reference .fasta file
+        n_jobs (int):
+        chromosomes (iterable):
+    Returns:
+        str:
+    """
+
+    args = [[bam_file_path, fasta_file_path, '-r {}'.format(c)]
+            for c in chromosomes]
+
+    multiprocess(call_variants_on_bam, args, n_jobs=n_jobs)
+
+    return
+
+
+def call_variants_on_bam(bam_file_path,
+                         fasta_file_path,
+                         regions=None,
+                         n_jobs=1):
     """
     Call variants on .bam file with freebayes.
     Arguments:
         bam_file_path (str):
         fasta_file_path (str): reference .fasta file
+        regions (str):
         n_jobs (int):
     Returns:
         str:
@@ -14,8 +43,14 @@ def call_variants_on_bam(bam_file_path, fasta_file_path, n_jobs=1):
 
     output_vcf_gz_file_path = bam_file_path + '.freebayes.vcf.gz'
 
-    command = 'freebayes --fasta-reference {} {} | bgzip -fc -@ {} > {}; tabix -f {}'.format(
+    additional_arguments = ''
+
+    if regions:
+        additional_arguments += '-r {}'.format(regions)
+
+    command = 'freebayes --fasta-reference {} {} {} | bgzip -fc -@ {} > {}; tabix -f {}'.format(
         fasta_file_path,
+        additional_arguments,
         bam_file_path,
         n_jobs,
         output_vcf_gz_file_path,
--- sequencing_process/process_vcf.py
+++ sequencing_process/process_vcf.py
@@ -1,5 +1,6 @@
 from os.path import join, split
 
+from . import CHROMOSOMES
 from .process_gz import bgzip, tabix
 from .support.support.subprocess_ import run_command
 
@@ -106,33 +107,7 @@ def bcftools_rename_chr(vcf_file_path, chromosome_map_file_path, n_jobs=1):
 
 
 def bcftools_extract_chromosomes(vcf_file_path,
-                                 chromosomes=[
-                                     '1',
-                                     '2',
-                                     '3',
-                                     '4',
-                                     '5',
-                                     '6',
-                                     '7',
-                                     '8',
-                                     '9',
-                                     '10',
-                                     '11',
-                                     '12',
-                                     '13',
-                                     '14',
-                                     '15',
-                                     '16',
-                                     '17',
-                                     '18',
-                                     '19',
-                                     '20',
-                                     '21',
-                                     '22',
-                                     'X',
-                                     'Y',
-                                     'MT',
-                                 ],
+                                 chromosome=CHROMOSOMES,
                                  n_jobs=1):
     """
     Extract chromosomes (chromosome 1 to 22, X, Y, and MT) from .vcf file.

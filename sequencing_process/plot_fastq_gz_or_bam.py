8e46f6de43cab9b70c7be7ffdc4abda5345c46b1
KwatME <kwatme8@gmail.com>
2017-11-11 14:04:42 -0800

    plot_fastq_gz_or_bam

--- sequencing_process/plot_fastq_gz_or_bam.py
+++ sequencing_process/plot_fastq_gz_or_bam.py
@@ -3,24 +3,24 @@ from os.path import isfile
 from .support.support.subprocess_ import run_command_and_monitor
 
 
-def plot_fastq_gzs_or_bams(fastq_gz_or_bam_file_paths, overwrite=False):
+def plot_fastq_gz_or_bam(fastq_gz_or_bam_file_path, overwrite=False):
     """
-    Plot .fastq.gz or .bam files.
+    Plot .fastq.gz or .bam file.
     Arguments:
-        fastq_gz_or_bam_file_paths (iterable):
+        fastq_gz_or_bam_file_path (str):
         overwrite (bool):
     Returns:
         str:
     """
 
-    output_path = fastq_gz_or_bam_file_paths[0] + '.plot'
+    output_path = fastq_gz_or_bam_file_path + '.plot'
 
     if isfile(output_path) and not overwrite:
         raise FileExistsError('{} exists.'.format(output_path))
 
     run_command_and_monitor(
-        'fastqp --output {0} --text {0} {1}'.format(
-            output_path, ' '.join(fastq_gz_or_bam_file_paths)),
+        'fastqp --output {0} --text {0} {1}'.format(output_path,
+                                                    fastq_gz_or_bam_file_path),
         print_command=True)
 
     return output_path

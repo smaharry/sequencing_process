11ac9f0376c05faa263b850fd07a43fcbca6b90d
KwatME <kwatme8@gmail.com>
2017-11-11 11:30:25 -0800

    test

--- sequencing_process/process_vcf_gz.py
+++ sequencing_process/process_vcf_gz.py
@@ -19,6 +19,7 @@ def concatenate_vcf_gzs_using_bcftools(vcf_gz_file_paths,
         str:
     """
 
+    print(dirname(vcf_gz_file_paths), stack()[0][3] + '.vcf')
     output_vcf_file_path = join(
         dirname(vcf_gz_file_paths), stack()[0][3] + '.vcf')
 

2a4c27e09471ccaa042bdf9dd9b13eec24c1e827
KwatME <kwatme8@gmail.com>
2017-11-13 11:38:42 -0800

    6

--- sequencing_process/make_reference_genome.py
+++ sequencing_process/make_reference_genome.py
@@ -1,4 +1,8 @@
-'GCA_000001405.15_GRCh38_full_analysis_set.fna.gz'
-'hs38DH-extra.fa'
-'GCA_000001405.15_GRCh38_full_plus_hs38DH-extra_analysis_set.fa'
-'gzip -dc {0} > {2}; cat {1} >> {2}; gzip {2}'
+full_name = 'GCA_000001405.15_GRCh38_full_analysis_set.fna.gz'
+extra_name = 'hs38DH-extra.fa'
+full_plus_extra_name = 'GCA_000001405.15_GRCh38_full_plus_hs38DH-extra_analysis_set.fa'
+
+download(''.format(full_name))
+download(''.format(extra_name))
+'gzip -dc {0} > {2}; cat {1} >> {2}; gzip {2}'.format(full_name, extra_name,
+                                                      full_plus_extra_name)

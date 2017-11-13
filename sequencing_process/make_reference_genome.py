187a1e5e9b8bc011be914e33b430908188cf9baa
KwatME <kwatme8@gmail.com>
2017-11-13 11:36:44 -0800

    5

---
+++ sequencing_process/make_reference_genome.py
@@ -0,0 +1,4 @@
+'GCA_000001405.15_GRCh38_full_analysis_set.fna.gz'
+'hs38DH-extra.fa'
+'GCA_000001405.15_GRCh38_full_plus_hs38DH-extra_analysis_set.fa'
+'gzip -dc {0} > {2}; cat {1} >> {2}; gzip {2}'

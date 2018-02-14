d6a042bcb14034b930c2e157904ad516f8fb292f
KwatME <kwatme8@gmail.com>
2018-02-14 14:34:28 -0800

    Version  suffix

--- sequencing_process/download_clinvar_vcf_gz.py
+++ sequencing_process/download_clinvar_vcf_gz.py
@@ -3,11 +3,12 @@ from os.path import exists, join
 from .support.support.network import download
 
 
-def download_clinvar_vcf_gz(directory_path, overwrite=False):
+def download_clinvar_vcf_gz(directory_path, version=None, overwrite=False):
     """
     Download the latest clinvar.vcf.gz.
     Arguments:
         directory_path (str):
+        version (str):
         overwrite (bool):
     Returns:
     """
@@ -16,8 +17,13 @@ def download_clinvar_vcf_gz(directory_path, overwrite=False):
     if not overwrite and exists(clinvar_vcf_gz_file_path):
         raise FileExistsError(clinvar_vcf_gz_file_path)
 
+    version_suffix = (
+        '_' + version,
+        '', )[version is None]
+
     for url in (
-            'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz',
-            'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi',
-    ):
+            'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar{}.vcf.gz'.
+            format(version_suffix),
+            'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar{}.vcf.gz.tbi'.
+            format(version_suffix), ):
         download(url, directory_path)

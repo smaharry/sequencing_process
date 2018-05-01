44c263bf7f2212b6b6634d31202a8cda79b8648b
KwatME <kwatme8@gmail.com>
2018-05-01 10:07:27 -0700

    0

--- sequencing_process/make_reference_genome.py
+++ sequencing_process/make_reference_genome.py
@@ -4,19 +4,22 @@ from . import RESOURCE_DIRECTORY_PATH
 from .print_and_run_command import print_and_run_command
 from .support.support.network import download
 
+# TODO: consider ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna.gz
+
 
 def make_reference_genome(directory_path, overwrite=False):
-
-    # TODO: consider getting just ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna.gz
 
     final_fa_gz_file_path = join(
         directory_path,
         'GCA_000001405.15_GRCh38_full_plus_hs38DH-extra_analysis_set.fa.gz')
+
     if not overwrite and exists(final_fa_gz_file_path):
+
         raise FileExistsError(final_fa_gz_file_path)
 
     fa_gz_file_path = join(directory_path,
                            'GCA_000001405.15_GRCh38_full_analysis_set.fna.gz')
+
     download(
         'ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/{}'.
         format(split(fa_gz_file_path)[-1]), directory_path)

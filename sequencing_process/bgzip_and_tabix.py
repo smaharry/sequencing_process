b58b7a6e5d28d9631a050ce829ba9bfa8117625e
KwatME <kwatme8@gmail.com>
2018-05-01 10:10:08 -0700

    1

--- sequencing_process/bgzip_and_tabix.py
+++ sequencing_process/bgzip_and_tabix.py
@@ -1,9 +1,15 @@
+from os.path import exists
+
 from .print_and_run_command import print_and_run_command
 
 
 def bgzip_and_tabix(file_path, n_job=1, overwrite=False):
 
     output_file_path = file_path + '.gz'
+
+    if not overwrite and exists(output_file_path):
+
+        raise FileExistsError(output_file_path)
 
     print_and_run_command(
         'bgzip --threads {0} {1} {2} && tabix {1} {3}'.format(

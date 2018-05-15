57adf41047f3b77504a265621b7974bb343eb620
KwatME <kwatme8@gmail.com>
2018-05-15 01:58:41 -0700

    20

--- sequencing_process/_print_and_run_command.py
+++ sequencing_process/_print_and_run_command.py
@@ -1,8 +1,8 @@
 from .support.support.subprocess_ import run_command
 
 
-def _print_and_run_command(*args, **kwargs):
+def _print_and_run_command(command):
 
     print()
 
-    return run_command(*args, print_command=True, **kwargs)
+    return run_command(command, print_command=True)
